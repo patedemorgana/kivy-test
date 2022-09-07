1bec967f1b0a688f23e944dbf541407ea9606519
patedemorgana <gabriel.volpate@gmail.com>
2022-09-07 12:25:47 -0300

    sg

---
+++ main.py
@@ -0,0 +1,23 @@
+## Boilerplate code
+
+# from kivy.app import App
+# from kivy.lang import Builder
+
+
+# GUI = Builder.load_file("screen.kv")
+
+# class MyAppp(App):
+    # def build(self):
+    #     return GUI
+
+## Boilerplate code
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+GUI = Builder.load_file("screen.kv")
+
+class MyAppp(App):
+    def build(self):
+        return GUI
